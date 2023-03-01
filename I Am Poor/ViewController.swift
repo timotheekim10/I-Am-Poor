c189b2cac4501e9e2389d2aeff82d6aa9b7d6de7
timotheekim10 <timotheekim10@gmail.com>
2023-03-01 14:31:26 +0900

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 티모시 킴 on 2023/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
